a8d3dfedc47ee88e589a4f4798130e71159ef84c
LunaIsCool <86474788+LunaIsCool@users.noreply.github.com>
2022-09-27 18:20:23 +0900

    Update main.c

--- LearnC/main.c
+++ LearnC/main.c
@@ -4,7 +4,14 @@
 int main(void)
 {	
 	startlogo();
-	startup();
+	int rep = 1;
+	do
+	{
+		startup();
+		printf("Do you want to play again? (Y = 1/ N = 2)\n\n\n");
+		scanf("%d", &rep);
+		
+	} while (rep == 1);
 	endlogo();
 	system("pause");
 	return 0;
